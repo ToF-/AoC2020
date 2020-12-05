0eda9806e92fa7b5ed859ebc77f51276ec35522f
ToF- <cthibaut@octo.com>
2020-12-05 19:28:59 +0100

    Day04a done

--- src/Day04a.hs
+++ src/Day04a.hs
@@ -3,18 +3,42 @@ module Day04a
 import Data.List
 import Text.ParserCombinators.ReadP
 import Data.Char
+import Data.Maybe
 
 type Field = (Key,Value)
 type Value = String
-data Key = BYR
-    deriving (Eq, Show)
+data Key = BYR | CID | ECL | EYR | HCL | HGT | IYR | PID
+    deriving (Eq, Show, Ord)
     
+validCount :: [String] -> Int
+validCount = length . filter (id) . map valid . collect 
+
+valid :: String -> Bool
+valid = validKeys . sort . map fst . fields 
+    where 
+        validKeys [BYR, CID, ECL, EYR, HCL, HGT, IYR, PID] = True
+        validKeys [BYR, ECL, EYR, HCL, HGT, IYR, PID] = True
+        validKeys _ = False
+
 parse = readP_to_S
 
+fields :: String -> [Field]
+fields = catMaybes . map field . words 
+
 field :: String -> Maybe Field
-field s = case parse byrField s of
+field s = case parse oneField s of
             [(f,_)] -> Just f
             [] -> Nothing
+
+oneField :: ReadP Field
+oneField = choice [byrField
+                  ,cidField
+                  ,eclField
+                  ,eyrField
+                  ,hclField
+                  ,hgtField
+                  ,iyrField
+                  ,pidField]
 
 satisfying :: ReadP a -> (a -> Bool) -> ReadP a
 satisfying parse predicate = do
@@ -25,9 +49,50 @@ satisfying parse predicate = do
 byrField :: ReadP Field
 byrField = do
     string "byr:"
-    -- s <- ((munch1 isDigit) `satisfying` ((==4).length))
+    s <- munch1 (const True)
     return (BYR,s)
 
+cidField :: ReadP Field
+cidField = do
+    string "cid:"
+    s <- munch1 (const True)
+    return (CID,s)
+
+eclField :: ReadP Field
+eclField = do
+    string "ecl:"
+    s <- munch1 (const True)
+    return (ECL,s)
+
+eyrField :: ReadP Field
+eyrField = do
+    string "eyr:"
+    s <- munch1 (const True)
+    return (EYR,s)
+
+iyrField :: ReadP Field
+iyrField = do
+    string "iyr:"
+    s <- munch1 (const True)
+    return (IYR,s)
+
+hclField :: ReadP Field
+hclField = do
+    string "hcl:"
+    s <- munch1 (const True)
+    return (HCL,s)
+
+hgtField :: ReadP Field
+hgtField = do
+    string "hgt:"
+    s <- munch1 (const True)
+    return (HGT,s)
+
+pidField :: ReadP Field
+pidField = do
+    string "pid:"
+    s <- munch1 (const True)
+    return (PID,s)
 
 collect :: [String] -> [String]
 collect = filter (/= "")
--- test/Day04aSpec.hs
+++ test/Day04aSpec.hs
@@ -2,7 +2,14 @@ module Day04aSpec
     where
 import Test.Hspec
 import Day04a
-
+-- byr (Birth Year)
+-- cid (Country ID)
+-- ecl (Eye Color)
+-- eyr (Expiration Year)
+-- hcl (Hair Color)
+-- hgt (Height)
+-- iyr (Issue Year)
+-- pid (Passport ID)
 spec = do
     describe "collect" $ do
         it "collects groups of lines separated by an empty line" $ do
@@ -21,9 +28,64 @@ spec = do
                 field "byr:1937" `shouldBe` Just (BYR,"1937") 
                 field "byr:1965" `shouldBe` Just (BYR,"1965") 
                 field "foo:1980" `shouldBe` Nothing 
---            it "if it has 4 digits" $ do
---                field "byr:foo" `shouldBe` Nothing 
---                field "byr:42" `shouldBe` Nothing 
 
+        describe "can be a cid" $ do
+            it "if it has a cid key" $ do 
+                field "cid:XX37" `shouldBe` Just (CID,"XX37") 
+                field "cid:XX65" `shouldBe` Just (CID,"XX65") 
+                field "foo:XX80" `shouldBe` Nothing 
 
-        
+        describe "can be a ecl" $ do
+            it "if it has a ecl key" $ do 
+                field "ecl:oth" `shouldBe` Just (ECL,"oth")
+
+        describe "can be a eyr" $ do
+            it "if it has a eyr key" $ do 
+                field "eyr:2030" `shouldBe` Just (EYR,"2030")
+
+        describe "can be a hcl" $ do
+            it "if it has a hcl key" $ do 
+                field "hcl:#abcdef" `shouldBe` Just (HCL,"#abcdef")
+
+        describe "can be a hgt" $ do
+            it "if it has a hgt key" $ do 
+                field "hgt:173cm" `shouldBe` Just (HGT,"173cm")
+
+        describe "can be a iyr" $ do
+            it "if it has a iyr key" $ do 
+                field "iyr:2015" `shouldBe` Just (IYR,"2015")
+
+        describe "can be a pid" $ do
+            it "if it has a pid key" $ do 
+                field "pid:123456789" `shouldBe` Just (PID,"123456789")
+    describe "password is valid" $ do
+        it "if it has all the keys" $ do
+            let s = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd  byr:1937 iyr:2017 cid:147 hgt:183cm valid"
+            valid s `shouldBe` True 
+            let t = "ecl:gry eyr:2020 hcl:#fffffd  byr:1937 iyr:2017 cid:147 hgt:183cm valid"
+            valid t `shouldBe` False
+
+        it "if it's missing only cid" $ do
+            let s = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd  byr:1937 iyr:2017 hgt:183cm valid"
+            valid s `shouldBe` True
+
+    describe "validCount" $ do
+        it "counts the valid passwords" $ do
+            let ls = ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd"
+                     ,"byr:1937 iyr:2017 cid:147 hgt:183cm"
+                     ,""
+                     ,"iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884"
+                     ,"hcl:#cfa07d byr:1929"
+                     ,""
+                     ,"hcl:#ae17e1 iyr:2013"
+                     ,"eyr:2024"
+                     ,"ecl:brn pid:760753108 byr:1931"
+                     ,"hgt:179cm"
+                     ,""
+                     ,"hcl:#cfa07d eyr:2025 pid:166559648"
+                     ,"iyr:2011 ecl:brn hgt:59in"]
+            validCount ls `shouldBe` 2 
+    it "all of this should solve the puzzle" $ do
+        p <- fmap lines $ readFile "./data/Day04input.txt"
+        validCount  p`shouldBe`  196
+
